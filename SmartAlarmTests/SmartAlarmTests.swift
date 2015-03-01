dedbead9e4402903e7e17a245004b057ada7712b
毕鸣 <BM@bimings-MacBook-Pro.local>
2015-03-01 20:47:55 +0800

    Initial Commit

---
+++ SmartAlarmTests/SmartAlarmTests.swift
@@ -0,0 +1,36 @@
+//
+//  SmartAlarmTests.swift
+//  SmartAlarmTests
+//
+//  Created by 毕鸣 on 1/3/15.
+//  Copyright (c) 2015 BM. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SmartAlarmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
