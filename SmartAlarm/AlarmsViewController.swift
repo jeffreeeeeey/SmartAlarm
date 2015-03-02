71205c276d296b969b90f94d4c3220a1aa1c947e
毕鸣 <BM@bimings-MacBook-Pro.local>
2015-03-03 06:42:17 +0800

    add clock

--- SmartAlarm/AlarmsViewController.swift
+++ SmartAlarm/AlarmsViewController.swift
@@ -17,7 +17,7 @@ class AlarmsViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationItem.leftBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ SmartAlarm/EditAlarmViewController.swift
@@ -0,0 +1,78 @@
+//
+//  EditAlarmViewController.swift
+//  SmartAlarm
+//
+//  Created by 毕鸣 on 2/3/15.
+//  Copyright (c) 2015 BM. All rights reserved.
+//
+
+import UIKit
+
+class EditAlarmViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var timePicker: UIPickerView!
+    
+    @IBOutlet weak var settingsTable: UITableView!
+    
+    private let hourComponent = 0
+    private let minuteComponent = 1
+    private let hours = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"]
+    private let minutes = ["00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelPressed(sender: AnyObject) {
+        let pre = self.presentingViewController as UIViewController!
+        
+        pre.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - Picker Data Source Methods
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == hourComponent {
+            return hours.count
+        } else {
+            return minutes.count
+        }
+    }
+    
+    //MARK: - Picker Delegate Methods
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        if component == hourComponent {
+            return hours[row]
+        } else {
+            return minutes[row]
+        }
+    }
+    
+    //MAEK: - Table View Data Source Methods
+    
+    
+
+
+}
